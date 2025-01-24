22b9c0f2c2a8b86c4403d6beb6c31480d7d77e48
Catalin <catalin.es@hotmail.com>
2025-01-24 11:44:44 +0200

    update

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,13 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
+    sourcemap: false, // Dezactivează maparea sursei
     rollupOptions: {
       output: {
         assetFileNames: ({ name }) => {
           if (/\.css$/.test(name ?? '')) {
             return 'assets/[name].[hash][extname]';
           }
-          return 'assets/[name][extname]'; // fără hash pentru fișierele de imagini
+          return 'assets/[name][extname]';
         },
       },
     },
