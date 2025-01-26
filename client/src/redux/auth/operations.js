519c1e74b8681d15b1cfc7e7d5d28f7621b42f71
Catalin <catalin.es@hotmail.com>
2025-01-26 08:06:22 +0200

    update

--- client/src/redux/auth/operations.js
+++ client/src/redux/auth/operations.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-//! ONRENDER:
-axios.defaults.baseURL = 'taskpro-server.onrender.com/taskpro';
 
-//! LOCAL:
-//axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
+
 
 const utils = {
   setAuthHeader: token =>
