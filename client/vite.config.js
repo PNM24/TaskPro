723b03369e895a4f9986ce0308e6b6db0b8702c9
Catalin <catalin.es@hotmail.com>
2025-01-26 08:12:33 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,8 +3,17 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'taskpro-server.onrender.com/taskpro',   // Adresa serverului back-end
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
   build: {
-    sourcemap: false, // Dezactivează maparea sursei
+    sourcemap: false, // Dezactivează maparea sursei pentru producție
     rollupOptions: {
       output: {
         assetFileNames: ({ name }) => {
